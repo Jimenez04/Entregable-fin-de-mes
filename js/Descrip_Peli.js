4506d91064e490163f6735c31355bace3a8c514c
José Enrique Jiménez Soto <jose.jimenezsoto18@ucr.ac.cr>
2021-06-28 20:26:13 -0600

    Nada

--- js/Descrip_Peli.js
+++ js/Descrip_Peli.js
@@ -3,33 +3,34 @@
  * @return String
  */
 
-var body2 =  document.getElementById("body2");
-var back =  document.getElementById("back");
+var body2 = document.getElementById("body2");
+var back = document.getElementById("back");
 // var url  = document.location.href = 'Descrip_Peli.html/id=section_movies';
 var prodId = getParameterByName('id');
 
 document.addEventListener("DOMContentLoaded", function () {
-   /*  var contenedor = document.createElement('div');
-    contenedor.classList.add('carousel','flex','flex_ajustable');
-    contenedor.setAttribute("id", element.name + "_js");
-    contenedor.setAttribute("value", element.id);
-    contenidocuerpo.append(contenedor); */
+    /*  var contenedor = document.createElement('div');
+     contenedor.classList.add('carousel','flex','flex_ajustable');
+     contenedor.setAttribute("id", element.name + "_js");
+     contenedor.setAttribute("value", element.id);
+     contenidocuerpo.append(contenedor); */
 
-   /*  var contenidocuerpo =  document.getElementById("cuerpo_js"); */
+    /*  var contenidocuerpo =  document.getElementById("cuerpo_js"); */
     var titulo = document.createElement('label');
     titulo.textContent = "Para los Playitos";
-    body2.append(titulo);   
-    console.log("El id es: "+prodId);
+    body2.append(titulo);
+    console.log("El id es: " + prodId);
+    alert("El ID es: " + prodId);
 }
 )
 
-back.addEventListener("click", function () { 
+back.addEventListener("click", function () {
     history.back();
 });
 
- function getParameterByName(name) {
+function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
+        results = regex.exec(location.search);
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
