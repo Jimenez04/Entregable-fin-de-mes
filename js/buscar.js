107c8cb89ec9206db27a6fce3914a9c3ef68ca5a
José Enrique Jiménez Soto <jose.jimenezsoto18@ucr.ac.cr>
2021-07-13 10:46:50 -0600

    Se actualiza el buscar

--- js/buscar.js
+++ js/buscar.js
@@ -1,5 +1,16 @@
+var url = "https://api.themoviedb.org/3/";
+ var key = '133e62f28b7a78182442c73f2c90e8b9';
+ var urlimg = 'http://image.tmdb.org/t/p/w500';
 
- var availableTags = [];
+ $(document).ready(function(){
+  $('img').each(function(){
+      if($(this)[0].naturalHeight == 0){
+           $(this).attr('src','../img/not-found.png');
+      }
+  });
+});
+
+var availableTags = [];
  $("#text_busqueda").keyup(function () {
    if ($("#text_busqueda").val().length>0) {
      fetch("https://api.themoviedb.org/3/search/movie?api_key=133e62f28b7a78182442c73f2c90e8b9&language=es&query=" +$("#text_busqueda").val())
@@ -14,28 +25,61 @@
        let pelicula = JSON.parse(resultadotext).results;
        pelicula.forEach(function (element)
             {       
-                availableTags.push({"label":element.title, "value":element.title, "id":element.id}
-                  );
+              availableTags.push({"label":element.title, "value":element.title, "id":element.id,   icon: element.poster_path});
            });
      })
      .catch( err => {
        console.log(err);
      });
-     $("#text_busqueda" ).autocomplete({source: availableTags, 
-     select: function( event, ui ) {
-       $(' #text_busqueda ' ).val(  ui.item.label ); 
-       $(' #id_Movie ').val( ui.item.id ); 
-       return false;
-    } ,
 
-  messages: {
-      noResults: '',
-      results: function() {}
-  }
-    });
+     $("#text_busqueda" ).autocomplete({
+      source: availableTags,
+      select: function( event, ui ) {
+        $(' #text_busqueda ' ).val(  ui.item.label ); 
+        $(' #id_Movie ').val( ui.item.id ); 
+        window.location = 'Descrip_Peli.html?id=' + ui.item.id;
+        return false;
+     }
+    })
+    //
+    $("#text_busqueda" ).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+      var $li = $('<li>'),
+          $img = $('<img>');
+try {
+        var image = new Image();
+        image.onload = function() {
+            // image exists and is loaded
+            $img.attr({
+              src: image.src,
+              alt: ""
+            });
+        }
+        image.onerror = function() {
+            var err = new Image();
+            err.src = '../img/not-found.png';
+            $img.attr({
+              src: err.src,
+              alt: ""
+            });
+        }
+        image.src = urlimg + item.icon;    
+} catch (error) {
+        $img.attr({
+          alt: ""
+        });
+}
+      $li.attr('data-value', item.label);
+      $li.append('<a href="#">');
+      $li.find('a').append($img).append(item.label);    
+      return $li.appendTo(ul);
+    };
    availableTags.length = 0;
    }
  });
+
+      
+
+
 
 function busquedapornombre() {
   var movieId = $("#id_Movie").val();
@@ -46,10 +90,7 @@ function busquedapornombre() {
       window.location = 'Descrip_Peli.html?id=' + movieId.toString();
     }else{
       try {
-        console.log(document.getElementsByTagName("ul")[1]
-        .getElementsByTagName("li")[0]
-        .getElementsByTagName("div")[0].textContent);
-        moviename = document.getElementsByTagName("ul")[1].getElementsByTagName("li")[0].getElementsByTagName("div")[0].textContent;
+        moviename = document.getElementsByTagName("ul")[1].getElementsByTagName("li")[0].getElementsByTagName("a")[0].textContent;
         if (moviename != "" && moviename.length > 0) {
           fetch("https://api.themoviedb.org/3/search/movie?api_key=133e62f28b7a78182442c73f2c90e8b9&language=es&query=" + moviename)
             .then( resultado => {
@@ -62,7 +103,6 @@ function busquedapornombre() {
             .then( resultadotext => {
               let pelicula = JSON.parse(resultadotext).results;
               var movieId = pelicula[0].id;
-              console.log(moviename);
               window.location = 'Descrip_Peli.html?id=' + movieId.toString();
             })
             .catch( err => {
@@ -87,42 +127,3 @@ function busquedapornombre() {
   }
   
 }
-
-
-
-
-
-// $('#text_busqueda').autocomplete({
-//   source : function(requete, reponse){
-//       $.ajax({
-//           url : "https://api.themoviedb.org/3/search/movie?api_key=133e62f28b7a78182442c73f2c90e8b9&language=es&",
-//           dataType : 'json', 
-//           data : {query : $('#text_busqueda').val(), maxRows : 15},
-//           success : function(donnee){
-//               reponse($.map(donnee, function(objet){
-//                 console.log(objet);
-//                   return {
-//                       label: objet.title,
-//                       value: objet.id
-//                       };
-//               }));
-//           }
-//       });
-//   },
-
-//   minLength: 3,
-//   delay:500,
-
-//   select: function( event, ui ) {
-//        $(' #text_busqueda ' ).val(  ui.item.label ); 
-//        $(' #id_personne ').val( ui.item.value ); 
-//        return false;
-//     } ,
-
-//   messages: {
-//       noResults: '',
-//       results: function() {}
-//   }
-
-
-// }); 
